a17470f9351e1097561c45a26c6b292af65d136c
BALASHEKHAR <kamandlabalashekhar@gmail.com>
2021-07-15 14:18:09 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //importing  states  from react
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 
 //importing logo
